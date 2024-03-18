6e1e88da73f1379b6f634356d0ef5790038aa7fc
Ichiro Hiraide <hiraide@atama.plus>
2024-03-18 13:45:38 +0900

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define REP(i,n) for(long long i=0;i<(n);i++)
+#define LL long long
+using namespace std;
+
+int main(void) {
+   // template
+   ios::sync_with_stdio(false);
+
+   return 0;
+}
